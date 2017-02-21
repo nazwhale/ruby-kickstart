92aabaae7a0cbf2c779f31384e0236a1e08e7656
Naz <nazmalik08@gmail.com>
2017-02-21 12:27:07 +0000

    s1c7 done

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,12 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  str = ""
+  string = string.chars
+  string[0..-2].each_with_index {|c,i| str << string[i+1] if c.downcase == "r"}
+  str
 end
