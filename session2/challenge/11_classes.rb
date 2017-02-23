7644ad97311714bb2d61a78fe47bc76d0c9e2c5b
Naz <nazmalik08@gmail.com>
2017-02-23 11:57:07 +0000

    s2c4 done

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -12,9 +12,75 @@
 # Zero bottles of beer on the wall.
 #
 # Your program should not use ninety-nine output statements!
-# Design your program with a class named BeerSong whose initialize method 
+# Design your program with a class named BeerSong whose initialize method
 # receives a parameter indicating the number of bottles of beer initially on the wall.
 # If the parameter is less than zero, set the number of bottles to zero. Similarly,
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+attr_accessor 'bottles'
+  def initialize(bottles)
+    if bottles < 0
+      bottles = 11
+    elsif bottles > 99
+      bottles = 99
+    end
+    self.bottles = bottles
+    print_song
+  end
+
+  def print_song
+    until @bottles == 1
+      num = englishNumber(@bottles)
+      puts "#{num.capitalize} bottles of beer on the wall,"
+      puts "#{num.capitalize} bottles of beer,"
+      puts "Take one down, pass it around,"
+      @bottles -= 1
+      num = englishNumber(@bottles)
+      puts "#{num.capitalize} bottles of beer on the wall."
+    end
+
+    puts "One bottle of beer on the wall,"
+    puts "One bottle of beer,"
+    puts "Take one down, pass it around,"
+    puts "Zero bottles of beer on the wall."
+  end
+
+  def englishNumber number
+    numString = ''
+
+    onesPlace = ['one',     'two',       'three',    'four',     'five',
+                 'six',     'seven',     'eight',    'nine']
+    tensPlace = ['Ten',     'Twenty',    'Thirty',   'Forty',    'Fifty',
+                 'Sixty',   'Seventy',   'Sighty',   'Ninety']
+    teenagers = ['Eleven',  'Twelve',    'Thirteen', 'Fourteen', 'Fifteen',
+                 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
+
+    remaining = number
+    write = remaining/10
+    remaining  = remaining - write*10
+
+    if write > 0
+      if ((write == 1) and (remaining > 0))
+        numString = numString + teenagers[remaining-1]
+        remaining = 0
+      else
+        numString = numString + tensPlace[write-1]
+      end
+      if remaining > 0
+        numString = numString + '-'
+      end
+    end
+
+    write = remaining  # How many ones remaining to write out?
+    remaining  = 0     # Subtract off those ones.
+
+    if write > 0
+      numString = numString + onesPlace[write-1]
+    end
+
+    numString
+  end
+end
