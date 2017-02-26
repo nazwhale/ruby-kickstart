44c9fdcd50d6d6817cc2e0ea62e671a1c650e6ea
Naz <nazmalik08@gmail.com>
2017-02-26 18:00:41 +0000

    s3c13 done

--- session3/challenge/13_proc.rb
+++ session3/challenge/13_proc.rb
@@ -52,5 +52,14 @@
 #   end
 # end       # => ["a", "m", "r", 1, 3, 4, 9, 2.5, 9.0, 25.8]
 
-def your_sort
+def your_sort(array, &sorter)
+  unless sorter
+    sorter = Proc.new {|a,b| a <=> b}
+  end
+  array.each_index do |i|
+    array.each_index do |v|
+      new = sorter.call(array[i], array[v])
+      array[i], array[v] = array[v], array[i] if new < 0
+    end
+  end
 end
